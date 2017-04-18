9af41902381bc6c8f7096b52e215508e3525e33b
Jacob Rief <jacob.rief@gmail.com>
2017-04-18 14:10:59 +0200

    emulate no migrations with None

--- treebeard/tests/settings.py
+++ treebeard/tests/settings.py
@@ -54,27 +54,17 @@ def get_db_conf():
 DATABASES = {'default': get_db_conf()}
 SECRET_KEY = '7r33b34rd'
 
-if DJANGO_VERSION < (1, 9):
+# This little hacks forces Django into the old syncdb behaviour,
+# creating models without migrations.
 
-    # This little hacks forces Django into the old syncdb behaviour,
-    # creating models without migrations.
+class DisableMigrations(object):
 
-    class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
 
-        def __contains__(self, item):
-            return True
-
-        def __getitem__(self, item):
+    def __getitem__(self, item):
+        if DJANGO_VERSION < (1, 9):
             return "notmigrations"
-
-
-    MIGRATION_MODULES = DisableMigrations()
-
-else:
-
-    MIGRATION_MODULES = {
-        'treebeard.tests': None,
-    }
 
 INSTALLED_APPS = [
     'django.contrib.auth',
